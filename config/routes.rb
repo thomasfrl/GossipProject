d2cb5f5a7ee54c9b1f6a7f71fdbe4ae4eb2b5562
thomasfrl <feral.thomas@gmail.com>
2019-02-04 22:44:07 +0100

    mise en page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'team', to: 'static_pages#team'
   get 'contact', to: 'static_pages#contact'
-  get 'welcome/:first_name', to: 'static_pages#welcome'
+  get 'welcome(/:first_name)', to: 'static_pages#welcome'
   get 'gossip/:id', to: 'static_pages#gossip'
   get 'user/:id', to: 'static_pages#user'
 
